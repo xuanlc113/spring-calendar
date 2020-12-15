76c0e00a7f2e3b28c47440e692d7a7453a2bb720
xuanlc113 <xuan.lc113@gmail.com>
2020-12-15 10:31:23 +0800

    add basic Auth0 authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { user, isAuthenticated, isLoading, loginWithRedirect, logout } = useAuth0();
+  console.log(user);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isAuthenticated ? <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Log Out
+    </button> : <button onClick={() => loginWithRedirect()}>Log In</button>}
     </div>
   );
 }
