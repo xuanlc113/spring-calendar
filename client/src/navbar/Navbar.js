54c371fb1b2f8aabf02e9c30ccbab443c9ebd2bb
xuanlc113 <xuan.lc113@gmail.com>
2020-12-17 10:24:51 +0800

    finish sidebar and nav

--- client/src/navbar/Navbar.js
+++ client/src/navbar/Navbar.js
@@ -1,31 +1,45 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button, Badge } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { Button, Badge, Select } from "antd";
+import { UserOutlined, LeftOutlined, RightOutlined } from "@ant-design/icons";
+import "./Navbar.css";
 
 export default function Navbar() {
+  const { Option } = Select;
   const { logout } = useAuth0();
   return (
     <div className="navbar">
-      <div
-        className="icon-friend"
-        style={{ margin: "0 1.5rem", padding: "0.5rem", borderRadius: "50%" }}
-      >
-        <Badge count={5}>
-          <UserOutlined
-            className="icon"
-            style={{
-              fontSize: "1.5rem",
-            }}
-          />
-        </Badge>
+      <div className="nav-date">
+        <LeftOutlined />
+        <h2>Day</h2>
+        <RightOutlined />
       </div>
-      <Button
-        type="primary"
-        size={"medium"}
-        onClick={() => logout({ returnTo: window.location.origin })}
-      >
-        Log Out
-      </Button>
+      <div className="nav-options">
+        <div>
+          <Select defaultValue="day">
+            <Option value="day">Day</Option>
+            <Option value="week">Week</Option>
+            <Option value="month">Month</Option>
+          </Select>
+        </div>
+        <div className="icon-friend">
+          <Badge count={5} size="small">
+            <UserOutlined
+              className="icon"
+              style={{
+                fontSize: "1.3rem",
+              }}
+            />
+          </Badge>
+        </div>
+        <Button
+          type="primary"
+          size="medium"
+          shape="round"
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Log Out
+        </Button>
+      </div>
     </div>
   );
 }
--- client/src/sidebar/Sidebar.js
+++ client/src/sidebar/Sidebar.js
@@ -1,12 +1,27 @@
 import Calendar from "react-calendar";
+import { Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import "./Sidebar.css";
 import "./ReactCalendar.css";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
+      <Button
+        type="primary"
+        size="large"
+        shape="round"
+        className="button-create"
+        icon={<PlusOutlined />}
+        style={{ fontSize: "1.2rem" }}
+      >
+        Create
+      </Button>
       <div className="date-selector">
         <Calendar />
+      </div>
+      <div className="contact">
+        <p>Calendars</p>
       </div>
     </div>
   );
